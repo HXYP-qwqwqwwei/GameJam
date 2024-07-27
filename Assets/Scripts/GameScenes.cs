29231d7aff9a4fe6335ea8f61a467e3b8c9d56a4
HXYP <qwqwqwwei@qq.com>
2024-07-28 01:04:09 +0800

    优化场景切换功能

---
+++ Assets/Scripts/GameScenes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScenes : MonoBehaviour
+{
+    public List<GameObject> mainScenes;
+
+    private int _currentSceneIndex;
+
+    public void NextScene()
+    {
+        GameObject currentScene = mainScenes[_currentSceneIndex];
+        _currentSceneIndex = (_currentSceneIndex + 1) % mainScenes.Count;
+        GameObject nextScene = mainScenes[_currentSceneIndex];
+        currentScene.SetActive(false);
+        nextScene.SetActive(true);
+    }
+    
+    public void PrevScene()
+    {
+        GameObject currentScene = mainScenes[_currentSceneIndex];
+        _currentSceneIndex = (_currentSceneIndex - 1) % mainScenes.Count;
+        GameObject nextScene = mainScenes[_currentSceneIndex];
+        currentScene.SetActive(false);
+        nextScene.SetActive(true);
+    }
+}
