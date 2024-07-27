59ce049a362ef74f629600f92fecd61f85d92cda
minzhipeng <minzhipeng@tuyoogame.com>
2024-07-27 14:35:24 +0800

    场景切换

---
+++ Assets/ContinueGameScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuToContinueGame : MonoBehaviour
+{
+    public void SceneSwitcher()
+    {
+        //SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Assets/MenuToGame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class MenuToGame : MonoBehaviour
+{
+
+    [SerializeField] private GameObject startGameScene;
+    [SerializeField] private GameObject continueGameScene;
+    [SerializeField] private GameObject settings;
+    public void Menu2Game(bool isActive)
+    {
+        startGameScene.SetActive(isActive);
+    }
+    
+    public void Menu2LoadedGame(bool isActive)
+    {
+        continueGameScene.SetActive(isActive);
+    }
+    
+    public void Menu2Settings(bool isActive)
+    {
+        settings.SetActive(isActive);
+    }
+}*/
